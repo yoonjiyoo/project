1a4119f9911919d0e74abc1c7baa715b01c03e87
mzeong <70924556+mzeong@users.noreply.github.com>
2023-06-09 23:51:35 +0900

    Update Detail.js

--- myapp/src/pages/Detail.js
+++ myapp/src/pages/Detail.js
@@ -10,157 +10,156 @@ import 'firebase/storage';
 
 // import MapContainer from "../App";
 
-function Detail(props) {
-  const { id } = useParams();
-  const 차량 = props.cars?.find((x) => x.idx === id);
-  const navigate = useNavigate();
-
-
-  const minDate = new Date(); // 예약 가능한 가장 빠른 날짜를 설정합니다.
-  const maxDate = new Date();
-  maxDate.setDate(maxDate.getDate() + 14); // 예약 가능한 가장 늦은 날짜를 설정합니다.
-  const [excludeDates, setExcludeDates] = useState(null);
-
-  // 솔리디티에서 예약 금지 배열 가져오기
-  // min~max 배열 만들기
-  // 두 배열에서 중복되는 원소 찾기 = 진정한 예약 금지 뱅려
-  // 해당 배열 excludeDates에 넣기 
-  // 달력 끝~ 이 상태에서 버튼 누르면 결제 창으로 넘어감
-  // 돈 보내면 차량의 예약 배열에 해당 날짜 값 넣어주기~
+const { kakao } = window;
+function MapContainer({searchPlace}) {
+  useEffect(()=>{
+    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    var options = { //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3 //지도의 레벨(확대, 축소 정도)
+    };
+    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
   
-  //const excludeDates = ['2023-05-21', '2023-05-30'];
-  // const excludeDates = ['2023-05-25', '2023-05-26'].map(dateString => parseISO(dateString));
-  //console.log(차량.carNumber);
-  //const excludeDates = props.contract.methods.getReservedDates(차량?.carNumber).call();
-  //console.log(excludeDates);
-  //setExcludeDates(excludeDates?.map(dateString => parseISO(dateString)));
-  //console.log(props.contract.methods.getReservedDates(차량.carNumber).call());
+    const ps = new kakao.maps.services.Places(); 
 
-  useEffect(() => {
-    async function getCount() {
-        if (props.contract) {
-            // setFilteredCars(props.cars); // 새로고침하면 빈 리스트됨 
-            // setFilteredCars(await props.contract.methods.getAllCars().call());
-            let temp = await props.contract.methods.getReservedDates(차량.carNumber).call();
-            setExcludeDates(temp.map(dateString => parseISO(dateString)));
+    ps.keywordSearch(searchPlace, placesSearchCB); 
+
+      function placesSearchCB (data, status, pagination) {
+        if (status === kakao.maps.services.Status.OK) {
+
+          var coords = new kakao.maps.LatLng(data[0].y, data[0].x);
+
+          var marker = new kakao.maps.Marker({
+              map: map,
+              position: coords
+          });
+
+          map.setCenter(coords);
+            // let bounds = new kakao.maps.LatLngBounds();
+
+            //     for (let i=0; i<data.length; i++) {
+            //         displayMarker(data[i]);    
+            //         bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+            //     }       
+
+                // map.setBounds(bounds);
+        } 
+    }
+
+        function displayMarker(place) {
+            let marker = new kakao.maps.Marker({
+                map: map,
+                position: new kakao.maps.LatLng(place.y, place.x) 
+            });
         }
-    }
-    getCount();
-    
-  db.collection('reviewCar').where("차량번호","==", 차량.carNumber).get().then((결과)=>{
-    
-    결과.forEach((doc)=>{
-        var 템플릿 = `<div>
-            <div>${doc.data().후기}</div>
-          </div>`;
-          document.getElementById("container mt-3").insertAdjacentHTML("afterend",
-          템플릿);
-          })
-          var 템플릿0 = `
-            <h3>후기 </h3>
-          `;
-    document.getElementById("container mt-3").insertAdjacentHTML("afterend",템플릿0);
-})
-db.collection('product').where("차량번호","==", 차량.carNumber).get().then((결과)=>{
-  결과.forEach((doc)=>{
-      var 템플릿 = `<div>
-          
-          <div>차량번호 : ${doc.data().차량번호}</div>
-          <div>모델명 : ${doc.data().모델명}</div>
-          <div>가능한 요일 : ${doc.data().요일}</div>
-          <div>주차 장소 : ${doc.data().주차장소}</div>
-          <div>대여료 : ${doc.data().대여료}</div>
-          <div>주행료 : ${doc.data().주행료}</div>
-          <div>등록날짜 : ${doc.data().날짜 && doc.data().날짜.toDate()}</div>
-          <div><img src=${doc.data().이미지} alt='이미지'/></div>
-        </div>`;
-        document.getElementById("container mt-3").insertAdjacentHTML("afterend",
-        템플릿);
-        })
-})
-  }, [props.contract]);
   
-
-  const [selectedDate, setSelectedDate] = useState(null);
-
-  function 시작종료배열로(minDate, maxDate) {
-    const dateArray = [];
-    let currentDate = minDate;
-  
-    while (currentDate <= maxDate) {
-      dateArray.push(currentDate.toISOString().split('T')[0]);
-      currentDate.setDate(currentDate.getDate() + 1);
-    }
-    console.log(dateArray);
-    return dateArray;
-  }
-  
-  function ReservationCalendar() {
-    // const [selectedDate, setSelectedDate] = useState(null);
-  
-    const isWeekdayDisabled = (date) => {
-      const day = date.getDay();
-      return props.cars[id].dayOfWeek & (1 << (6-day));
-      //return day === 0 || day === 6;
-    };
-  
-    const isReservedDateDisabled = (date) => {
-      
-
-      //console.log(props.contract.methods.getReservedDates().call());
-      //const reserveDates = props.contract.methods.getReservedDates().call();
-      //return ['2023-05-01', '2023-05-10', '2023-05-15'];
-      //const reservedDates = ['2023-05-01', '2023-05-10', '2023-05-15'];
-
-      //const formattedDate = date.toISOString().split('T')[0];
-      //return reservedDates.includes(formattedDate);
-    };
-
-  
-    return (
-      <div>
-        <h1>예약 달력</h1>
-        <DatePicker
-          selected={selectedDate}
-          onChange={(date) => setSelectedDate(date)}
-          filterDate={isWeekdayDisabled}
-          excludeDates={excludeDates}
-          minDate={minDate}
-          maxDate={maxDate}
-          dateFormat="yyyy-MM-dd"
-        />
-      </div>
-    );
-  }
-
-
-  const send = async () => {
-    // const accounts = await props.web3.eth.getAccounts();
-      // await props.web3.eth.sendTransaction({
-      //   from: accounts[0],
-      //   to: props.cars[id].owner,
-      //   value: props.web3.utils.toWei(props.web3.utils.BN(props.cars[id].rentalFee), "ether")
-      // });
-      
-
-      // 달력에서 가능한 날만 선택 가능하게 ...
-      navigate("/search/"+id+"/payment", { state : { selectedDate : selectedDate} });
-    };
+  }, [searchPlace]);
 
   return (
-    <div>
-      <ReservationCalendar/>
-      {props.cars !== undefined ? (
-        <div>
+      <div id="map" style={{
+        width: '500px',
+        height: '400px'
+      }}></div>
+  )
+}
 
-          <h1>{차량?.modelName}</h1>
-          <p><span>{차량?.carNumber}</span></p>
-          <div id="container mt-3"></div> 
-          <button onClick={send}>예약하기</button>
+function Detail(props) {
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+  const selectedCar = props.cars?.find((x) => x.idx === id);
+
+  const minDate = new Date();
+  const maxDate = new Date(minDate.getTime() + 14 * 24 * 60 * 60 * 1000);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [excludeDates, setExcludeDates] = useState(null);
+
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+      if (selectedCar) {
+          try {
+              const fetchExcludeDates = async() => {
+                  if (props.contract) {
+                      let dates = await props.contract.methods.getAllRenDates(selectedCar.carNumber).call();
+                      setExcludeDates(dates.map(dateString => parseISO(dateString)));
+                  }
+              }
+
+              const fetchImage = () => {
+                  db.collection('product')
+                      .where("차량번호","==", selectedCar.carNumber)
+                      .get()
+                      .then((result) => {
+                          if (!result.empty) {
+                              const doc = result.docs[0];
+                              const imageUrl = doc.data().이미지; 
+                              setImageUrl(imageUrl);
+                          }
+                      });
+              }
+
+              db.collection('reviewCar').where("차량번호","==", selectedCar.carNumber).get().then((결과)=>{
+    
+                결과.forEach((doc)=>{
+                    var 템플릿 = `<div>
+                        <div>${doc.data().후기}</div>
+                      </div>`;
+                      document.getElementById("container mt-3").insertAdjacentHTML("afterend",
+                      템플릿);
+                      })
+                      var 템플릿0 = `
+                        <h3>후기 </h3>
+                      `;
+                document.getElementById("container mt-3").insertAdjacentHTML("afterend",템플릿0);
+            })
+
+              fetchExcludeDates();
+              fetchImage();
+          } catch(e) {
+              console.log(e);
+          }
+      }
+  }, [selectedCar]);
+
+  
+  function ReservationCalendar() {
+      const filterUnavailableDates = (date) => {
+          const day = date.getDay();
+          return props.cars[id].dayOfWeek & (1 << (6-day));
+      };
+  
+      return (
+        <div>
+          <DatePicker
+            selected={selectedDate}
+            onChange={(date) => setSelectedDate(date)}
+            filterDate={filterUnavailableDates}
+            excludeDates={excludeDates}
+            minDate={minDate}
+            maxDate={maxDate}
+            dateFormat="yyyy-MM-dd"
+          />
         </div>
-      ) : (
-        null
-      )}
+      );
+  }
+
+  return (
+      <div>
+          <div>
+              {imageUrl && <img src={imageUrl} alt="차량 이미지" style={{ width: "90%" }}/>}
+          </div>
+
+          <h1>{selectedCar?.modelName}</h1>
+          <span>{selectedCar?.owner}</span>
+          
+          <MapContainer searchPlace={selectedCar?.location} />
+
+          <ReservationCalendar/>
+          <button onClick={() => {
+              navigate("/search/"+id+"/payment", { state : { selectedDate : selectedDate} });
+          }}>예약하기</button>
+          <div id="container mt-3"></div> 
     </div>
   );
 }
